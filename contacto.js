bb48034998dab71ee88a3f07235286454240dee2
MarionaPros <202208187@alu.comillas.edu>
2025-04-26 19:03:38 +0200

    Commit de cositas que faltaban en el html y subir en javascript

---
+++ contacto.js
@@ -0,0 +1,25 @@
+document.getElementById("formulario").addEventListener("submit",async function (event){
+    event.preventDefault();
+    const mensajeObtenido={
+        nombre:document.getElementById("nombre").value,
+        email:document.getElementById("email").value,
+        mensaje:document.getElementById("mensaje").value
+    };
+    try{
+        const response=await fetch('http://localhost:8080/api/sinusuario/contacto',{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(mensajeObtenido)
+        });
+        if(response.ok){
+            alert("Mensaje enviado correctamente");
+            document.getElementById("formulario").reset();
+        }else{
+            alert("Ha ocurrido un error al enviar el mensaje");
+        }
+    }catch(error){
+        console.error("Ha ocurrido un error al enviar el mensaje",error);
+    }
+});
